4c3d87b000528101f240753e23bda7280960ac6c
nakasyou <79000684+nakasyou@users.noreply.github.com>
2023-01-15 14:14:48 +0900

    Create mouse.js

---
+++ src/mouse.js
@@ -0,0 +1,12 @@
+export default class Mouse extends Pointer{
+  constructor(){
+    super("mouse");
+    this.down=false;
+  }
+  _down(){
+    this.down=true;
+  }
+  _up(){
+    this.down=false;
+  }
+}
